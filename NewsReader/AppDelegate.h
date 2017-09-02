62b292d8efc59139f5075971abcb9659e11cb437
zhukang <zhukang@zhukangdemac.local>
2017-09-02 19:39:27 +0800

    Initial Commit

---
+++ NewsReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsReader
+//
+//  Created by zhukang on 17/9/2.
+//  Copyright © 2017年 zhukang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsReader
+//
+//  Created by zhukang on 17/9/2.
+//  Copyright © 2017年 zhukang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
